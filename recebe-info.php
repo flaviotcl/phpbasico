e999ae921086dfc9099d85f4c1d5d276bcb21938
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-14 17:39:42 -0300

    e

---
+++ recebe-info.php
@@ -0,0 +1,13 @@
+<?php
+// HTTP  GET e POST. 
+//    http://localhost:8008/?nome=luiz
+
+//var_dump($_GET);
+//var_dump($_POST);
+
+?>
+
+
+<h1><?php //echo $_GET['nome']; ?></h1>
+
+<h1><?php echo $_POST['nome']; ?></h1>
