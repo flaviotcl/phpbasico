b2d929a171ff0b92553bf2479514b7956ef06300
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-14 14:03:47 -0300

    Strings

---
+++ strings.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$string1 = 'Scholl of Net';
+$string2 = 'Iniciando com PHP';
+
+echo "... ".$string1." ... --- ".$string2." ---";
+
+echo "\n";
+
+echo "    Luiz"." Carlos";
+
+echo "\n";
+
+$string3 = "... ".$string1." ... --- ".$string2." ---";
+echo $string3;
+
+echo "\n";
+
+$string4 = "... $string1 ... --- $string2 ---";
+
+echo $string4;
+
+echo "\n";
+
+
+
+
+?>
