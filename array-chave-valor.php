400341a9c1c500eb202122fdc9f79056afe98fd6
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-14 14:44:46 -0300

    Array c/ Chave + Valor

---
+++ array-chave-valor.php
@@ -0,0 +1,26 @@
+<?php
+
+/************** Declaração de Array na forma Explícita **************/
+$array = array(
+    "Luiz" => 40,
+    200,
+    100,
+    3000 => 150,
+    "School of Net",
+    true
+);
+echo "\n";
+
+
+echo $array['Luiz'];
+echo "\n";
+
+echo $array[1];
+echo "\n";
+
+
+echo $array[3000];
+echo "\n";
+
+
+var_dump($array);
