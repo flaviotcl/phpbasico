9854f1001c2f638d29511a30cf38b546b6c9ca22
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-14 14:14:08 -0300

    Precedência

---
+++ precedencia.php
@@ -0,0 +1,23 @@
+<?php
+
+$soma = 5 + 3 * 8;
+$soma = 5 + 24;
+$soma = 29;
+
+
+// Esquerda ----------->>>>>>>> Direita
+
+
+$string = "Scholl of Net"."Iniciando com PHP";
+$string = "Scholl of NetIniciando com PHP";
+echo "\n";
+
+echo 5 + 3 * 8;
+
+echo "\n";
+
+$soma = (5 + 3 * 5) * 8;
+
+echo $soma;
+
+echo "\n";
