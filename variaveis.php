cc55ebe1a422f910980843c5a8d16e89792ae98d
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-13 22:54:24 -0300

    Variaveis e Tipo de dados

---
+++ variaveis.php
@@ -0,0 +1,38 @@
+<?php
+$nome = "School of net";
+
+echo $nome;
+
+echo "\n";
+
+$nome = "Iniciando com PHP !";
+
+echo $nome;
+
+echo "\n";
+
+$numero = 5000;
+
+
+echo $numero;
+
+echo "\n";
+
+
+$primeiroNome = "Luiz";
+$primeiro_Nome =   'Luiz';
+
+
+$variavelTrue = true;
+$variavelFalse = false;
+echo "\n";
+echo $variavelTrue;
+echo "\n";
+echo $variavelFalse;
+echo "\n";
+
+
+
+
+
+?>
