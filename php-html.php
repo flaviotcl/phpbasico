28b062d3199cc54154ce101856b51766b8fab24f
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-14 17:03:59 -0300

    PHP c/  HTML

---
+++ php-html.php
@@ -0,0 +1,18 @@
+<?php
+    $nome = "Flávio Tavares";
+    $html = "Iniciando com PHP";
+?>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <?php  
+            echo 5 * 3;
+            echo "<br>";
+            echo $nome; 
+            echo "<br>";
+        ?>        
+        <?php echo $html; ?>
+    </body>
+</html>
